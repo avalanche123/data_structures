6e870c242a6e685c87c671b3e90ddc499f81948a
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-05-17 08:27:52 -0400

    initial commit

--- test/graph.js
+++ test/graph.js
@@ -20,55 +20,26 @@ turns = {
   ED: intersection.vertix('ED')
 }
 
-turns.AB.edge(turns.EA);
-turns.AB.edge(turns.DA);
-turns.AB.edge(turns.BC);
-turns.AB.edge(turns.BD);
-
-turns.AC.edge(turns.EA);
-turns.AC.edge(turns.DA);
-turns.AC.edge(turns.DB);
-turns.AC.edge(turns.EB);
-turns.AC.edge(turns.BD);
-
-turns.AC.edge(turns.EA);
-turns.AC.edge(turns.EB);
-turns.AC.edge(turns.EC);
-
-turns.BC.edge(turns.AB);
-turns.BC.edge(turns.DB);
-turns.BC.edge(turns.EB);
-
-turns.BD.edge(turns.AB);
-turns.BD.edge(turns.AC);
-turns.BD.edge(turns.DA);
-turns.BD.edge(turns.EB);
-turns.BD.edge(turns.EC);
-
-turns.DA.edge(turns.AB);
-turns.DA.edge(turns.AC);
-turns.DA.edge(turns.BD);
-turns.DA.edge(turns.EC);
-turns.DA.edge(turns.EB);
-
-turns.DB.edge(turns.AC);
-turns.DB.edge(turns.BC);
-turns.DB.edge(turns.EC);
-
-turns.EA.edge(turns.AB);
-turns.EA.edge(turns.AC);
-turns.EA.edge(turns.AD);
-
-turns.EB.edge(turns.DA);
-turns.EB.edge(turns.BC);
-turns.EB.edge(turns.AC);
-turns.EB.edge(turns.AD);
-turns.EB.edge(turns.BD);
-
-turns.EC.edge(turns.DA);
-turns.EC.edge(turns.DB);
-turns.EC.edge(turns.BD);
-turns.EC.edge(turns.AD);
+intersection.edge(turns.AB, turns.EA);
+intersection.edge(turns.AB, turns.DA);
+intersection.edge(turns.AB, turns.BC);
+intersection.edge(turns.AB, turns.BD);
+intersection.edge(turns.AC, turns.EA);
+intersection.edge(turns.AC, turns.DA);
+intersection.edge(turns.AC, turns.DB);
+intersection.edge(turns.AC, turns.EB);
+intersection.edge(turns.AC, turns.BD);
+intersection.edge(turns.AD, turns.EA);
+intersection.edge(turns.AD, turns.EB);
+intersection.edge(turns.AD, turns.EC);
+intersection.edge(turns.BC, turns.DB);
+intersection.edge(turns.BC, turns.EB);
+intersection.edge(turns.BD, turns.DA);
+intersection.edge(turns.BD, turns.EB);
+intersection.edge(turns.BD, turns.EC);
+intersection.edge(turns.DA, turns.EC);
+intersection.edge(turns.DA, turns.EB);
+intersection.edge(turns.DB, turns.EC);
 
 console.log(intersection.greedy().map(function(vertix) {
   return vertix.name;
